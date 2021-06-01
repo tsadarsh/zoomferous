23b15100689562c6979938398c6678bc8c52654b
Adarsh TS <tsadarsh0707@gmail.com>
2021-06-01 22:12:06 +0530

    Select color to mask option added

--- rgb_mask.py
+++ rgb_mask.py
@@ -5,6 +5,10 @@ import cv2 as cv
 class App:
 	def __init__(self):
 		self.cid = 0
+		self.frame = None
+		self.ink_color = []
+		self.lower_color = [110,50,50]
+		self.upper_color = [130,255,255]
 
 	def start_camera(self):
 		cap = cv.VideoCapture(self.cid)
@@ -13,9 +17,10 @@ class App:
 			exit()
 
 		while True:
-			frame = self.read_from_camera(cap)
-			cv.imshow('frame', frame)
-			self.show_green_only(frame)
+			self.frame = self.read_from_camera(cap)
+			cv.imshow('frame', self.frame)
+			self.show_green_only(self.frame)
+			self.set_mouse_event()
 
 	def stop_camera(self, cap):
 		cap.release()
@@ -36,18 +41,24 @@ class App:
 
 	def show_green_only(self, frame):
 		hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-		lower_blue = np.array([110,50,50])
-		upper_blue = np.array([130,255,255])
 
-
-		lower_limit = np.array([0, 0, 0])
-		upper_limit = np.array([50, 255, 50])
-
-		mask = cv.inRange(hsv, lower_blue, upper_blue)
+		mask = cv.inRange(hsv, np.array(self.lower_color), np.array(self.upper_color))
 		masked_frame = cv.bitwise_and(frame, frame, mask=mask)
 		cv.imshow('mask', mask)
-		cv.imshow('green_masked_frame', masked_frame)
+		cv.imshow('color_masked_frame', masked_frame)
 
+	def set_mouse_event(self):
+		cv.setMouseCallback('frame', self.get_color_from_click)
+
+	def get_color_from_click(self, event, x, y, flags, param):
+		if event == cv.EVENT_LBUTTONDBLCLK:
+			frame_as_list = self.frame[y, x].tolist()
+			frame_in_hsv = cv.cvtColor(np.uint8([[frame_as_list]]), cv.COLOR_BGR2HSV)
+			self.ink_color = frame_in_hsv.tolist()[0][0]
+
+			self.lower_color[0] = (self.ink_color[0]-10) % 180
+			self.upper_color[0] = (self.ink_color[0]+10) % 180
+			print("setting", self.lower_color, "to", self.upper_color)
 
 app = App()
 app.start_camera()

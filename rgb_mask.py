08835382b1b5341c3cbe63b8f5c98e956abbf623
Adarsh TS <tsadarsh0707@gmail.com>
2021-06-02 11:20:31 +0530

    modify videofeed_draw to include rgb_mask

--- rgb_mask.py
+++ rgb_mask.py
@@ -19,8 +19,8 @@ class App:
 		while True:
 			self.frame = self.read_from_camera(cap)
 			cv.imshow('frame', self.frame)
-			self.show_green_only(self.frame)
-			self.set_mouse_event()
+			self.show_masked_frame(self.frame)
+			#self.set_mouse_event()
 
 	def stop_camera(self, cap):
 		cap.release()
@@ -39,7 +39,7 @@ class App:
 
 		return frame
 
-	def show_green_only(self, frame):
+	def show_masked_frame(self, frame):
 		hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
 		mask = cv.inRange(hsv, np.array(self.lower_color), np.array(self.upper_color))
@@ -51,14 +51,14 @@ class App:
 		cv.setMouseCallback('frame', self.get_color_from_click)
 
 	def get_color_from_click(self, event, x, y, flags, param):
-		if event == cv.EVENT_LBUTTONDBLCLK:
-			frame_as_list = self.frame[y, x].tolist()
+		if event == cv.EVENT_MBUTTONDBLCLK:
+			frame_as_list = param[0][y, x].tolist()
 			frame_in_hsv = cv.cvtColor(np.uint8([[frame_as_list]]), cv.COLOR_BGR2HSV)
 			self.ink_color = frame_in_hsv.tolist()[0][0]
 
 			self.lower_color[0] = (self.ink_color[0]-10) % 180
 			self.upper_color[0] = (self.ink_color[0]+10) % 180
-			print("setting", self.lower_color, "to", self.upper_color)
+			print("picking", self.lower_color, "to", self.upper_color, f"from {x}, {y}")
 
-app = App()
-app.start_camera()
+# app = App()
+# app.start_camera()
--- videofeed_draw.py
+++ videofeed_draw.py
@@ -1,16 +1,21 @@
 import numpy as np
 import cv2 as cv
 
+from rgb_mask import App
+
 
 class Freecom:
 	def __init__(self, cid=0):
 		self.cid = cid
 		self.cam_width = None
 		self.cam_height = None
+		self.frame = None
+		self.ink_color = []
+		self.lower_color = [110,50,50]
+		self.upper_color = [130,255,255]
 		self.perspective_crop = []
-		self.mask_color_lower = [0, 0, 0]
-		self.mask_color_upper = [255, 255, 255]
-		self.mask_color_width = 10
+		self.blank_img = None
+		self.rgb_mask = App()
 
 	def start_cam(self):
 		cap = cv.VideoCapture(self.cid)
@@ -18,76 +23,52 @@ class Freecom:
 			print("Cannot open camera")
 			exit()
 
-		# Get camera's width and height
 		self.cam_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 		self.cam_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-		# Black background from points
-		blank_img = np.zeros((self.cam_height, self.cam_width, 3), np.uint8)
-
-		#create trackbar
-		cv.namedWindow('mask_settings')
-		cv.createTrackbar('H', 'mask_settings', 0, 360, self.update_hue_mask_value)
-		cv.createTrackbar('S', 'mask_settings', 0, 100, self.update_sat_mask_value)
-		cv.createTrackbar('V', 'mask_settings', 0, 100, self.update_val_mask_value)
-		cv.createTrackbar('W', 'mask_settings', 0, 50, self.update_hsv_mask_width)
+		self.blank_img = np.zeros((self.cam_height, self.cam_width, 3), np.uint8)
 
 		while True:
-			ret, frame = cap.read()
+			self.frame = self.read_from_camera(cap)
 
-			if not ret:
-				print("Cant receive frame..")
-				break
+	def read_from_camera (self, cap):
+		ret, frame = cap.read()
 
-			if len(self.perspective_crop) < 4:
-				cv.imshow('feed', frame)
-				cv.imshow('bg', blank_img)
-				param = [blank_img]
-				cv.setMouseCallback('feed', self.draw_point, param)
-			else:
-				pts1 = np.float32(self.perspective_crop)
-				pts2 = np.float32([[0, 0], [self.cam_width, 0], [0, self.cam_height], [self.cam_width, self.cam_height]])
-				transform_frame_matrix = cv.getPerspectiveTransform(pts1, pts2)
-				transform_frame = cv.warpPerspective(frame, transform_frame_matrix, (self.cam_width, self.cam_height))
-				cv.imshow('new', transform_frame)
-				mask_frame = self.show_masked(transform_frame)
-				cv.imshow('masked', mask_frame)
-			if cv.waitKey(1) == ord('q'):
-				self.stop_cam(cap)
-				break
+		if not ret:
+			print("Can't recieve frame..")
+			self.stop_camera(cap)
 
-	def stop_cam(self, cap):
+		if len(self.perspective_crop) < 4:
+			cv.imshow('feed', frame)
+			cv.imshow('bg', self.blank_img)
+			param = [self.blank_img]
+			cv.setMouseCallback('feed', self.draw_point, param)
+		else:
+			pts1 = np.float32(self.perspective_crop)
+			pts2 = np.float32([[0, 0], [self.cam_width, 0], [0, self.cam_height], [self.cam_width, self.cam_height]])
+			transform_frame_matrix = cv.getPerspectiveTransform(pts1, pts2)
+			transform_frame = cv.warpPerspective(frame, transform_frame_matrix, (self.cam_width, self.cam_height))
+			cv.imshow('new', transform_frame)
+
+			self.rgb_mask.show_masked_frame(transform_frame)
+			param = [transform_frame]
+			cv.setMouseCallback('new', self.rgb_mask.get_color_from_click, param)
+
+		if cv.waitKey(1) == ord('q'):
+			self.stop_camera(cap)
+
+		return frame
+
+	def stop_camera(self, cap):
 		cap.release()
 		cv.destroyAllWindows()
+		exit()
 
 	def draw_point(self, event, x, y, flags, param):
 		if event == cv.EVENT_LBUTTONDBLCLK:
-			print(f'event triggered at {x},{y}')
+			print(f'drawing point at {x},{y}')
 			self.perspective_crop.append([x, y])
 			cv.circle(param[0], (x, y), 10, (255, 0, 0), -1)
-
-	def update_hsv_mask_width(self, val):
-		self.mask_color_width = val
-
-	def update_hue_mask_value(self, val):
-		self.mask_color_lower[0] = val-self.mask_color_width
-		self.mask_color_upper[0] = val+self.mask_color_width
-
-	def update_sat_mask_value(self, val):
-		self.mask_color_lower[1] = val-self.mask_color_width
-		self.mask_color_upper[1] = val+self.mask_color_width
-
-	def update_val_mask_value(self, val):
-		self.mask_color_lower[2] = val-self.mask_color_width
-		self.mask_color_upper[2] = val+self.mask_color_width
-
-	def show_masked(self, frame):
-		#frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-		lower_black = np.array(self.mask_color_lower)
-		upper_black = np.array(self.mask_color_upper)
-		mask = cv.inRange(frame, lower_black, upper_black)
-		res = cv.bitwise_and(frame, frame, mask=mask)
-		return mask
 
 
 app = Freecom()

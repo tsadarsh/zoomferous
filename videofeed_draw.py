cd8248cbe01975e2329f2f9b572af9d493481b02
Adarsh TS <tsadarsh0707@gmail.com>
2021-06-11 09:19:33 +0530

    Refactor - 2

--- videofeed_draw.py
+++ videofeed_draw.py
@@ -29,7 +29,11 @@ class Freecom:
 		self.corner_points_overlay = self.create_blank_overlay(self.cam_height, self.cam_width)
 
 		while True:
-			self.read_from_camera(cap)
+			self.frame = self.read_from_camera(cap)
+			if len(self.perspective_crop) < 4:
+				self.selecting_corner_points()
+			else:
+				self.show_transformed_frame()
 			self.check_for_quit_program(cap)
 
 	def check_for_quit_program(self, cap):
@@ -44,37 +48,36 @@ class Freecom:
 
 	def read_from_camera (self, cap):
 		ret, frame = cap.read()
-		self.frame = frame
 
 		if not ret:
 			print("Can't recieve frame..")
 			self.stop_camera(cap)
 
-		if len(self.perspective_crop) < 4:
-			frame = self.frame
-			corner_points_overlay_gray = cv.cvtColor(self.corner_points_overlay, cv.COLOR_BGR2GRAY)
-			ret, corner_points_overlay_mask = cv.threshold(corner_points_overlay_gray, 10, 255, cv.THRESH_BINARY)
-			inv_corner_points_overlay_mask = cv.bitwise_not(corner_points_overlay_mask)
-			frame_for_corner_points_overlay = cv.bitwise_and(frame, frame, mask=inv_corner_points_overlay_mask)
-			frame_overlayed_corner_points = cv.add(frame_for_corner_points_overlay, self.corner_points_overlay)
+		return frame
 
-			cv.imshow('feed', frame_overlayed_corner_points)
-			
-			param = [self.corner_points_overlay]
-			cv.setMouseCallback('feed', self.draw_point, param)
-		else:
-			cv.destroyWindow('feed')
+	def selecting_corner_points(self):
+		corner_points_overlay_gray = cv.cvtColor(self.corner_points_overlay, cv.COLOR_BGR2GRAY)
+		ret, corner_points_overlay_mask = cv.threshold(corner_points_overlay_gray, 10, 255, cv.THRESH_BINARY)
+		inv_corner_points_overlay_mask = cv.bitwise_not(corner_points_overlay_mask)
+		frame_for_corner_points_overlay = cv.bitwise_and(self.frame, self.frame, mask=inv_corner_points_overlay_mask)
+		frame_overlayed_corner_points = cv.add(frame_for_corner_points_overlay, self.corner_points_overlay)
 
-			pts1 = np.float32(self.perspective_crop)
-			pts2 = np.float32([[0, 0], [self.cam_width, 0], [0, self.cam_height], [self.cam_width, self.cam_height]])
-			transform_frame_matrix = cv.getPerspectiveTransform(pts1, pts2)
-			transform_frame = cv.warpPerspective(frame, transform_frame_matrix, (self.cam_width, self.cam_height))
-			cv.imshow('transformed', transform_frame)
+		cv.imshow('Zoomferous', frame_overlayed_corner_points)
 
-			mask, color_masked = self.rgb_mask.show_masked_frame(transform_frame)
-			cv.imshow('mask', mask)
-			param = [transform_frame]
-			cv.setMouseCallback('transformed', self.rgb_mask.get_color_from_click, param)
+		param = [self.corner_points_overlay]
+		cv.setMouseCallback('Zoomferous', self.draw_corner_points, param)
+
+	def show_transformed_frame(self):
+		pts1 = np.float32(self.perspective_crop)
+		pts2 = np.float32([[0, 0], [self.cam_width, 0], [0, self.cam_height], [self.cam_width, self.cam_height]])
+		transform_frame_matrix = cv.getPerspectiveTransform(pts1, pts2)
+		transform_frame = cv.warpPerspective(self.frame, transform_frame_matrix, (self.cam_width, self.cam_height))
+		cv.imshow('Zoomferous', transform_frame)
+
+		mask, color_masked = self.rgb_mask.show_masked_frame(transform_frame)
+		cv.imshow('mask', mask)
+		param = [transform_frame]
+		cv.setMouseCallback('Zoomferous', self.rgb_mask.get_color_from_click, param)
 
 	def stop_camera(self, cap):
 		cap.release()
@@ -84,7 +87,7 @@ class Freecom:
 	def create_blank_overlay(self, height, width):
 		return np.zeros((height, width, 3), np.uint8)
 
-	def draw_point(self, event, x, y, flags, param):
+	def draw_corner_points(self, event, x, y, flags, param):
 		if event == cv.EVENT_LBUTTONDOWN:
 			print(f'drawing point at {x},{y}')
 			self.perspective_crop.append([x, y])

3086a7507aec77fd903f26b20eeaee48919d3c24
soumilbaldota <71919921+soumilbaldota@users.noreply.github.com>
2021-06-12 11:05:45 +0530

    sauvola update
    uncomment to use and comment other threshold

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from videofeed_draw import Freecom
+from videofeed_draw import Freecom, Mask_from_ink_color
 import key_bindings
-
-
+import cv2 as cv
+import os
 app = Freecom()
+a=Mask_from_ink_color()
+cv.namedWindow('mask')
+cv.createTrackbar('k','mask',0,500,app.masker.nothing)
 
 while True:
 	app.run_zoomferous()
